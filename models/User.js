9e6a3f1d7cb122b15eae3baa9ea50b441c7d520e
Dinuth Diruksha <dinuthk69@gmail.com>
2025-03-01 01:54:59 +0530

    latest

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-//import validator from "validator";
+import validator from "validator";
+import bcrypt from "bcryptjs";
 
 
 const UserSchema = new mongoose.Schema({
@@ -12,13 +13,34 @@ const UserSchema = new mongoose.Schema({
         required: [true, "Email is required"],
         unique: true,
         lowercase: true,
-        //validator: [validator.isEmail, "Please provide a valid email"]
+        validator: [validator.isEmail, "Please provide a valid email"]
     },
     password: {
         type: String,
         required: [true, "Password is required"],
         minlength: 6
-    }
+    },
+    // confirmPassword: {
+    //     type: String,
+    //     required: [true, "Confirm Password is required"],
+    //     validate: {
+    //         validator: function (value) {
+    //             return value === this.password;
+    //         },
+    //         message: "Passwords do not match"
+    //     }
+    // }
+});
+//encrypt password
+UserSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next(); //if password is not modified, return next
+
+    const salt = await bcrypt.genSalt(10);
+    //encrypt password before saving
+    this.password = await bcrypt.hash(this.password, salt);
+    
+    //this.confirmPassword = undefined;
+    next();
 });
 
 const User = mongoose.model("User", UserSchema);
